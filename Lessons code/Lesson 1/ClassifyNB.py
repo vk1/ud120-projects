e179ca5ad35fcfb1fe215ad8f32a6c71eb18f551
Vaibhav Kumar <vk1@users.noreply.github.com>
2016-02-06 20:24:55 -0800

    Added lesson 2 SVM approach to lesson code quiz

--- Lessons code/Lesson 1/ClassifyNB.py
+++ Lessons code/Lesson 1/ClassifyNB.py
@@ -1,5 +1,8 @@
 from sklearn.naive_bayes import GaussianNB
 
+# Lesson 2 SVM included
+# from sklearn import svm
+
 def classify(features_train, labels_train):   
     ### import the sklearn module for GaussianNB
     ### create classifier
@@ -10,5 +13,9 @@ def classify(features_train, labels_train):
     ### your code goes here!
     clf = GaussianNB()
     clf.fit(features_train, labels_train)
+
+    # SVM code
+    # clf = svm.SVC()
+    # clf.fit(features_train, labels_train)
     
     return clf
