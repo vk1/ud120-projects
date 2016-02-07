a5f551a3f061f9bc967225b6dfe5afcc26fc0a87
Vaibhav Kumar <vk1@users.noreply.github.com>
2016-02-06 20:46:47 -0800

    Lesson 2 mini-project completed

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,28 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
+clf = SVC(kernel='rbf', C=10000)
+
+# Slice the data to use only small subset
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+
+# Fit the data
+t0 = time()
+clf.fit(features_train, labels_train)
+print 'Training time: {0} s'.format(round(time()-t0, 3))
+
+# Predict 
+t1 = time()
+pred = clf.predict(features_test)
+print 'Prediction time: {0} s'.format(round(time()-t1, 3))
+
+# Check accuracy
+accuracy = accuracy_score(labels_test, pred)
+print 'Accuracy: {0}'.format(round(accuracy, 4))
 #########################################################
 
 
