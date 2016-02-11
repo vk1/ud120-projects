438f287a06a2b61a554de923df64175a6e54aa45
Vaibhav Kumar <vk1@users.noreply.github.com>
2016-02-10 23:09:26 -0800

    Lesson 4 complete.

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -3,6 +3,7 @@
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+from time import time
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -24,21 +25,39 @@ plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
 plt.legend()
 plt.xlabel("bumpiness")
 plt.ylabel("grade")
-plt.show()
+# plt.show()
 ################################################################################
 
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
+# Select one of the following classifers. Comment out the rest.
+clf = KNeighborsClassifier(n_neighbors=4) # k-Nearest Neighbors
+# clf = AdaBoostClassifier() # AdaBoost Classifier
+# clf = RandomForestClassifier() # Random Forest Classifier
 
+# Fit the data
+t0 = time()
+clf = clf.fit(features_train, labels_train)
+print 'Training time: {0} s'.format(round(time()-t0, 3))
 
+# Predict
+t1 = time()
+labels_pred = clf.predict(features_test)
+print 'Prediction time: {0} s'.format(round(time()-t1, 3))
 
+# Check accuracy
+accuracy = accuracy_score(labels_test, labels_pred)
+print 'Accuracy: {0}'.format(round(accuracy, 4))
 
-
-
-
+# Plot
 try:
     prettyPicture(clf, features_test, labels_test)
+    plt.show()
 except NameError:
-    pass
+	print 'prettyPicture passed'
+	pass
